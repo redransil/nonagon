f7bfde2c8401337e90eb68641417f8acc1c111cf
Red <aransil@gmail.com>
2024-02-13 14:35:06 -0800

    added app

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+//express server
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
